30b6290bb093e2302df8d09c842d61dcbd917005
Luca Memini <lmemini@malcert.com>
2019-07-30 06:46:29 +0000

    export_host.sh - added check process alredy running
    reload_bin.sh
    - added check for error in config file

--- scripts/export_host.sh
+++ scripts/export_host.sh
@@ -4,27 +4,32 @@ source /etc/bind/misp.config
 
 # non toccare!! :-)
 DOMAIN_URL="/attributes/text/download/domain"
+PCOUNT=`pgrep -xc export_host.sh`
 
-
-if [ -z $AUTH_KEY ]; then
-        logger -it misp_exporter "Disabled";
-        exit 0;
+if [ "$PCOUNT" -gt "1" ]; then
+        logger -it misp_exporter "Alredy running";
 else
-        #curl -v -H "Authorization: $AUTH_KEY" -H "Accept: application/json" -H "Content-Type: application/json" $HOST/events/1
-        domain=`curl -k -s -H "Authorization: $AUTH_KEY" -H "Accept: application/json" -H "Content-Type: application/json" $HOST$DOMAIN_URL | sort | uniq`
+        if [ -z $AUTH_KEY ]; then
+                logger -it misp_exporter "Disabled";
+                exit 0;
+        else
+                #curl -v -H "Authorization: $AUTH_KEY" -H "Accept: application/json" -H "Content-Type: application/json" $HOST/events/1
+                domain=`curl -k -s -H "Authorization: $AUTH_KEY" -H "Accept: application/json" -H "Content-Type: application/json" $HOST$DOMAIN_URL | sort | uniq`
+        fi
+
+        tot=0;
+
+        echo "" > /etc/bind/named.conf.sinkhole.swp
+
+        for i in $domain; do
+                echo "zone \"$i\" {type master; notify no; file \"/etc/bind/blockeddomains.db\";};" >> /etc/bind/named.conf.sinkhole.swp
+                tot=$((tot+1))
+        done
+
+        logger -it misp_exporter "Exported $tot domains";
+
+        cp /etc/bind/named.conf.sinkhole /etc/bind/named.conf.sinkhole.save
+        mv /etc/bind/named.conf.sinkhole.swp /etc/bind/named.conf.sinkhole
+
 fi
-
-tot=0;
-
-echo "" > /etc/bind/named.conf.sinkhole.swp
-
-for i in $domain; do
-	echo "zone \"$i\" {type master; notify no; file \"/etc/bind/blockeddomains.db\";};" >> /etc/bind/named.conf.sinkhole.swp
-	tot=$((tot+1))
-done
-
-logger -it misp_exporter "Exported $tot domains";
-
-cp /etc/bind/named.conf.sinkhole /etc/bind/named.conf.sinkhole.save
-mv /etc/bind/named.conf.sinkhole.swp /etc/bind/named.conf.sinkhole
 
--- scripts/reload_bind.sh
+++ scripts/reload_bind.sh
@@ -4,30 +4,34 @@ fname="/etc/bind/named.conf.sinkhole"
 fhash="/etc/bind/.named.conf.sinkhole.md5"
 
 if [ ! -f $fhash ]; then
-	if [ -f $fname ]; then
-		md5sum $fname > $fhash
-		rndc reload
-	else
-		echo "$fname not found"
-		exit 1
-	fi
+        if [ -f $fname ]; then
+                md5sum $fname > $fhash
+                /usr/sbin/rndc reload 1>/dev/null 2>/dev/null
+        else
+                echo "$fname not found"
+                exit 1
+        fi
 fi
 
 if ! md5sum --status -c $fhash; then
-	logger -it "reload_bind" "$fname hash changed, reloading bind"
-	diff -Nura /etc/bind/named.conf.sinkhole.save /etc/bind/named.conf.sinkhole
-	md5sum $fname > $fhash
-	/usr/sbin/named-checkconf
-	if [ $? == 0 ]; then
-		/usr/sbin/rndc reload
-		/usr/sbin/rndc flush
-	else
-		logger -it "reload_bind" "ERROR: Invalid config file"
-	fi
+        logger -it "reload_bind" "$fname hash changed"
+        # diff -Nura /etc/bind/named.conf.sinkhole.save /etc/bind/named.conf.sinkhole
+        md5sum $fname > $fhash
+
+        #/usr/sbin/named-checkconf
+        ERR=`/usr/sbin/named-checkconf -z 1>/dev/null`
+        if [ $? == 0 ]; then
+                logger -it "reload_bind" "Reloading bind server with new fresh zones"
+        else
+                 #/usr/sbin/named-checkconf -z | grep -v loaded
+                echo $ERR
+                logger -it "reload_bind" "WARNING: Error are present in config file, reloading could have problems"
+        fi
+        /usr/sbin/rndc reload 1>/dev/null 2>/dev/null
+        /usr/sbin/rndc flush
 else
-	logger -it "reload_bind" "nothing to do..."
-	exit 0
+        logger -it "reload_bind" "nothing to do..."
+        exit 0
 fi
 
 exit
-
